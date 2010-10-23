13f814eb8de60624aacde5ee41a24d01ee8c7e95
leeht2008 <leeht2008@b5af6838-721d-4da8-22b2-bb3ad3b07076>
2010-10-23 20:00:10 +0000

    lihaitao_20101024_new file

--- src/MCU/time_manager.h
+++ src/MCU/time_manager.h
@@ -1,3 +1,17 @@
 /* 时间转换
    每隔1s调用func()增加时间 
 */
+#ifndef __TIME_MANAGER_H__
+#define __TIME_MANAGER_H__
+
+#ifdef __cpluscplus
+    extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
