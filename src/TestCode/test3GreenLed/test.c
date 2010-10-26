0b169af18351cff2e6e1e0c535afdc8e3d1b239e
hustgxc <hustgxc@b5af6838-721d-4da8-22b2-bb3ad3b07076>
2010-10-26 09:37:21 +0000

    add two testPrj in TestCode dir

---
+++ src/TestCode/test3GreenLed/test.c
@@ -0,0 +1,15 @@
+org 0000h
+
+SETB P0.6
+d:LCALL delay
+CPL P0.6
+JMP d
+
+delay:
+	MOV R0,#250
+	d1:MOV R1,#250
+	d2:DJNZ R1,d2
+	DJNZ R0,d1
+RET
+
+end
---
+++ src/TestCode/testLEDMatrix/testMatrix.c
@@ -0,0 +1,42 @@
+#include <reg52.h>
+
+typedef unsigned char data byte;
+
+sbit ledLineA = P2^4;
+sbit ledLineB = P2^3;
+sbit ledLineC = P2^2;
+sbit ledLineD = P2^1;
+sbit ledSwitch = P2^0;
+
+void delay(byte time)
+{
+    byte i;
+    do
+    {
+        for (i= 0; i < 250; i++)
+            ;
+    } while (--time != 0);
+}
+
+void lighting(byte lineNum)
+{
+    ledLineD = (lineNum & 1) ? 1 : 0;
+    ledLineC = (lineNum & 2) ? 1 : 0;
+    ledLineB = (lineNum & 4) ? 1 : 0;
+    ledLineA = (lineNum & 8) ? 1 : 0;
+}
+
+void main()
+{
+    byte lightLine = 0;
+
+    ledSwitch = 1;
+    
+    while (1)
+    {
+        if (++lightLine == 16)
+            lightLine = 0;
+        lighting(lightLine);
+        delay(200);
+    }
+}
